07185d7794714d84553737a7c56bd4718046aff0
kwziatek <wziatek.karol04@gmail.com>
2026-06-19 11:54:08 +0200

    lista5 init

--- sw/lista5/main/main.ino
+++ sw/lista5/main/main.ino
@@ -1,46 +1,89 @@
-#include <Servo.h> //Biblioteka odpowiedzialna za serwa
+#include <Servo.h>
 #include "LiquidCrystal_I2C.h"
 #include "Wheels.h"
+#include "PinChangeInterrupt.h"
 
 byte LCDAddress = 0x27;
 
 LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
 Wheels w;
 
- 
-Servo serwomechanizm;  //Tworzymy obiekt, dzięki któremu możemy odwołać się do serwa 
-int pozycja = 90; //Aktualna pozycja serwa 0-180
-int zmiana = 10; //Co ile ma się zmieniać pozycja serwa?
- 
-void setup() 
-{ 
-  serwomechanizm.attach(11);  //Serwomechanizm podłączony do pinu 9
-} 
- 
-// void loop() 
-// {  
-//   if (pozycja < 180) { //Jeśli pozycja mieści się w zakresie
-//     serwomechanizm.write(pozycja); //Wykonaj ruch
-//   } else { //Jeśli nie, to powrót na początek
-//     pozycja = 0;
-//   }    
+//piny silników
+#define EnA 5
+#define In1 7
+#define In2 8
+#define In3 2
+#define In4 4
+#define EnB 3
+
+// piny dla sonaru (HC-SR04)
+#define TRIG A4
+#define ECHO A5
+
+// piny dla interrupt
+#define INTINPUT0 A0
+#define INTINPUT1 A1
+
+// pin kontroli serwo (musi być PWM)
+#define SERVO 11
+
+Servo serwo;
+
+void setup() {
+  w.attach(In4, In3, EnB, In2, In1, EnA);
+  pinMode(TRIG, OUTPUT);    // TRIG startuje sonar
+  pinMode(ECHO, INPUT);     // ECHO odbiera powracający impuls
+
+  Serial.begin(9600);
+
+  serwo.attach(SERVO);
+
+/* rozejrzyj się w zakresie od 0 stopni (patrz na jedną burtę)
+ *  do 180 stopni (patrz na prawą burtę). Wydrukuj na konsoli
+ *  kierunek patrzenia i najbliżej widziany obiekt (pojedynczy pomiar)
+ */
+  for(byte angle = 0; angle < 180; angle+= 20) {
+    lookAndTellDistance(angle);
+    delay(500);
+  }
   
-//   pozycja = pozycja + zmiana; //Zwiększenie aktualnej pozycji serwa
-//   delay(500); //Opóźnienie dla lepszego efektu                        
-// }
+/* patrz przed siebie */
+  serwo.write(90);
+
+}
 
 void loop() 
-{
-  pozycja = pozycja + zmiana;
-  serwomechanizm.write(pozycja);
-  delay(300);
-  pozycja = pozycja - zmiana;
-  serwomechanizm.write(pozycja);
-  delay(300);
-  pozycja = pozycja - zmiana;
-  serwomechanizm.write(pozycja);
-  delay(300);
-  pozycja = pozycja + zmiana;
-  serwomechanizm.write(pozycja);
-  delay(300);
+{ 
+  w.setSpeed(200);
+  w.forward();
+  delay(1000);
+  w.stop();
+  delay(500);
+  
+}
+
+void lookAndTellDistance(byte angle) {
+  
+  unsigned long tot;      // czas powrotu (time-of-travel)
+  unsigned int distance;
+
+  Serial.print("Patrzę w kącie ");
+  Serial.print(angle);
+  serwo.write(angle);
+  
+/* uruchamia sonar (puls 10 ms na `TRIGGER')
+ * oczekuje na powrotny sygnał i aktualizuje
+ */
+  digitalWrite(TRIG, HIGH);
+  delay(10);
+  digitalWrite(TRIG, LOW);
+  tot = pulseIn(ECHO, HIGH);
+
+/* prędkość dźwięku = 340m/s => 1 cm w 29 mikrosekund
+ * droga tam i z powrotem, zatem:
+ */
+  distance = tot/58;
+
+  Serial.print(": widzę coś w odległości ");
+  Serial.println(distance);
 }
